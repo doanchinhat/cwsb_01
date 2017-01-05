1738fbffddaf711d1a6252757a86ce2e3a8ca460
Doan Chi Nhat <doan.chi.nhat@framgia.com>
2017-01-05 09:45:08 +0700

    Finish filter on map

--- db/schema.rb
+++ db/schema.rb
@@ -265,6 +265,7 @@ ActiveRecord::Schema.define(version: 20161223014352) do
     t.datetime "deleted_at"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "price_type_id"
     t.index ["booking_type_id"], name: "index_prices_on_booking_type_id", using: :btree
     t.index ["deleted_at"], name: "index_prices_on_deleted_at", using: :btree
     t.index ["space_id"], name: "index_prices_on_space_id", using: :btree
